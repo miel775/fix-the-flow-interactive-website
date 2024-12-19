ae9b91fc5da384ab8ba856140d6fdc895e2e2828
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2024-12-19 09:58:45 +0100

    last update before the wrap up
    De popup werkt nu naar behoren

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
 
-// let buttons = document.querySelectorAll('button'); // Stap 1: Selecteer alle buttons
-
-// // hier wordt er een klikfunctie toegevoegd op alle buttons
-// buttons.forEach(button => {
-//   button.addEventListener('click', function () {
-//     // Alle planten worden geselecteerd
-//     let articles = document.querySelectorAll('.schaduwplant');
-//     let halfschaduw = document.querySelectorAll('.halfschaduwplant');
-//     let zon = document.querySelectorAll('.zonplant');
-    
-//     // de artikelen worden toegevoegd met de klasse schaduwplant
-//     articles.forEach(article => {
-//     // er is een specifieke active klasse gemaakt van alle soorten planten
-//       article.classList.add('active');
-//     });
-
-//     halfschaduw.forEach(halfschaduwp => {
-//       // er is een specifieke active klasse gemaakt van alle soorten planten
-//         article.classList.remove('active');
-//       });
-//   });
-// });  
-// alle buttons
-
+// alle parameters
 let article1 = document.querySelector('.halfschaduwplant1')
 let popup1 = document.querySelector('.popup1')
 let closebttn = document.querySelector('.close')
-// alle parameters
+let textelement = document.querySelectorAll('.textelement article')
+let buttons = document.querySelectorAll('.buttons button')
+
 
 // om de popup te activeren
    article1.addEventListener('click', function(){
@@ -37,6 +16,33 @@ let closebttn = document.querySelector('.close')
 // de afsluitknop om de popup af te sluiten
       popup1.classList.remove('active');
    });
+
+
+// alle textelement articles worden getoond
+   function showArticle(articleNumber) {
+
+      // er wordt in de textelement CSS gezegd dat alle textelementen niet getoond worden
+      textelement.forEach(article => article.style.display = 'none');
+
+      // de textelement wordt geselecteerd door middel van een id toe te voegn -X
+      let selectedArticle = document.querySelector(`#textelement-${articleNumber}`);
+      // als de article is geslecteerd wordt die textelement article getoond
+      if (selectedArticle) {
+         selectedArticle.style.display = 'block';
+      }
+   }
+
+// er wordt op elke button ene click element toegevoegd
+buttons.forEach(button => {
+   button.addEventListener('click', () => {
+      // deArticlenumber wordt gelinkt aan de data-textelement nummer met het article number
+      let articleNumber = button.getAttribute('data-textelement');
+      showArticle(articleNumber);
+   });
+});
+
+// normaal wordt de eerste article getoond
+showArticle(1);
 
    
 
